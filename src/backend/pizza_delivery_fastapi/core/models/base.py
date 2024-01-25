9e9b9fc2abdc61650f5afac0393a6cfcf94a53d7
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2024-01-25 18:39:46 +0300

    Configured UUID data type for postgresql and sqlite
    Changes to be committed:
    	modified:   src/backend/pizza_delivery_fastapi/core/models/base.py

--- src/backend/pizza_delivery_fastapi/core/models/base.py
+++ src/backend/pizza_delivery_fastapi/core/models/base.py
@@ -1,9 +1,46 @@
+import uuid
 from sqlalchemy.orm import (
     Mapped,
     DeclarativeBase,
     mapped_column,
     declared_attr,
 )
+from sqlalchemy.types import TypeDecorator, CHAR
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class GUID(TypeDecorator):
+    """Platform-independent GUID type.
+    Uses PostgreSQL's UUID type, otherwise uses
+    CHAR(32), storing as stringified hex values.
+    """
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(UUID())
+        else:
+            return dialect.type_descriptor(CHAR(32))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == 'postgresql':
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return "%.32x" % uuid.UUID(value).int
+            else:
+                # hexstring
+                return "%.32x" % value.int
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            if not isinstance(value, uuid.UUID):
+                value = uuid.UUID(value)
+            return value
 
 
 class Base(DeclarativeBase):
@@ -13,4 +50,8 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[GUID] = mapped_column(
+        GUID(),
+        primary_key=True,
+        default=lambda: str(uuid.uuid4()),
+    )
