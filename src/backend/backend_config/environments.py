72db55d4db05b42076a71c16d9c53faf252905b0
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2024-01-25 18:30:04 +0300

    add environments from sqlite
    Changes to be committed:
    	modified:   src/backend/backend_config/environments.py

--- src/backend/backend_config/environments.py
+++ src/backend/backend_config/environments.py
@@ -1,5 +1,4 @@
 import os
-
 from dotenv import load_dotenv
 
 from backend_config import dotenv_path
@@ -7,9 +6,10 @@ from backend_config import dotenv_path
 
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-    POSTGRES_HOST: str = os.environ.get("POSTGRES_LOCAL")
-else:
-    POSTGRES_HOST: str = os.environ.get("POSTGRES_HOST")
+    # POSTGRES_HOST: str = os.environ.get("POSTGRES_LOCAL")  # activated if local db postgres
+
+
+POSTGRES_HOST: str = os.environ.get("POSTGRES_HOST")
 
 LOG_LEVEL: str = os.environ.get("LOG_LEVEL")
 PORT: int = int(os.environ.get("BACKEND_PORT_OUT"))
@@ -21,4 +21,7 @@ POSTGRES_PASSWORD: str = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_PORT: int = int(os.environ.get("POSTGRES_PORT"))
 POSTGRES_DB: str = os.environ.get("POSTGRES_DB")
 
-DB_URL_REAL: str = f'{SCHEMAS}://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}'
+if os.path.exists(dotenv_path):
+    DB_URL_REAL: str = "sqlite+aiosqlite:///sqlite.db"  # activated if local db sqlite
+else:
+    DB_URL_REAL: str = f'{SCHEMAS}://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}'
