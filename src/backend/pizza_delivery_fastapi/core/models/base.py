b49f38bf4c8136d1958571ffd9006dfdaf237777
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2024-01-26 19:53:46 +0300

    add Base Models
    Changes to be committed:
    	modified:   src/backend/pizza_delivery_fastapi/core/models/base.py

--- src/backend/pizza_delivery_fastapi/core/models/base.py
+++ src/backend/pizza_delivery_fastapi/core/models/base.py
@@ -1,46 +1,11 @@
-import uuid
+from datetime import datetime
+from sqlalchemy import func
 from sqlalchemy.orm import (
     Mapped,
     DeclarativeBase,
     mapped_column,
     declared_attr,
 )
-from sqlalchemy.types import TypeDecorator, CHAR
-from sqlalchemy.dialects.postgresql import UUID
-
-
-class GUID(TypeDecorator):
-    """Platform-independent GUID type.
-    Uses PostgreSQL's UUID type, otherwise uses
-    CHAR(32), storing as stringified hex values.
-    """
-    impl = CHAR
-
-    def load_dialect_impl(self, dialect):
-        if dialect.name == 'postgresql':
-            return dialect.type_descriptor(UUID())
-        else:
-            return dialect.type_descriptor(CHAR(32))
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return value
-        elif dialect.name == 'postgresql':
-            return str(value)
-        else:
-            if not isinstance(value, uuid.UUID):
-                return "%.32x" % uuid.UUID(value).int
-            else:
-                # hexstring
-                return "%.32x" % value.int
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return value
-        else:
-            if not isinstance(value, uuid.UUID):
-                value = uuid.UUID(value)
-            return value
 
 
 class Base(DeclarativeBase):
@@ -50,8 +15,14 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
 
-    id: Mapped[GUID] = mapped_column(
-        GUID(),
-        primary_key=True,
-        default=lambda: str(uuid.uuid4()),
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.utcnow,
+        nullable=False,
+    )
+    update_at: Mapped[datetime] = mapped_column(
+        onupdate=func.now(),
+        default=datetime.utcnow,
+        nullable=False,
     )
