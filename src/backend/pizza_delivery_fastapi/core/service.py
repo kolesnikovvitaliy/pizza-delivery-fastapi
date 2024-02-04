276049ea9c9f1a4b20b05bbe0f75f0375d7b5c99
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2024-02-04 18:24:32 +0300

    add service db
    Changes to be committed:
    	new file:   src/backend/pizza_delivery_fastapi/core/exceptions.py
    	new file:   src/backend/pizza_delivery_fastapi/core/service.py

---
+++ src/backend/pizza_delivery_fastapi/core/service.py
@@ -0,0 +1,46 @@
+from typing import Any
+
+from sqlalchemy import Insert, Result, Select, Update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def session_close(session: AsyncSession) -> None:
+    await session.commit()
+    await session.close()
+
+
+async def fetch_one_all(
+    select_query: Select | Insert | Update, session: AsyncSession, all: bool = False
+) -> Any | None:
+    result: Result = await session.execute(select_query)
+    await session_close(session=session)
+    if all:
+        return result.scalars().all()
+    return result.scalars().first()
+
+
+async def execute_add(items: Any, session: AsyncSession) -> Any:
+    session.add(items)
+    await session.commit()
+    await session.refresh(items)
+    await session.close()
+    return items
+
+
+async def execute_delete(model: Any, value: Any, session: AsyncSession) -> None:
+    await session.delete(model, value)
+    await session_close(session=session)
+
+
+async def execute_get(model: Any, value: Any, session: AsyncSession) -> Any:
+    result: Result = await session.get(model, value)
+    await session_close(session=session)
+    return result
+
+
+async def execute(
+    select_query: Insert | Update,
+    session: AsyncSession,
+) -> None:
+    await session.execute(select_query)
+    await session_close(session=session)
